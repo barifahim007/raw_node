d99761bf650f42296870507939a2baddd9aad7fb
fahim007 <ehsanulbarifahim7@gmail.com>
2024-03-09 01:45:32 +0600

    day-7 finalyy finished the project

--- index.js
+++ index.js
@@ -1,33 +1,21 @@
-/*
- * Project: uptime monitoring api
- * Description: a uptime monitoring api that check the active links
- * Authore: fahim007
- * Date: 01/03.2024
- */
-
 // dependencies
-const http = require("node:http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environments = require("./helpers/evniroments");
-const data = require("./lib/data");
-const { sendTwilioSms } = require("./helpers/notifications");
+const server = require("./lib/server");
+const worker = require("./lib/worker");
 
 // app object-module scaffolding
 const app = {};
 
-//  @twiloo sms
-sendTwilioSms("01735789091", "hey twlio brozz", (error) => {
-  console.log(`this is twlior borz ${error}`);
-});
-//  create server - take an function
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(environments.port, () => {
-    console.log(`the server is runing in ${environments.port}`);
-  });
+// call the app function
+
+app.init = () => {
+  // start the server
+  server.init();
+
+  //  start the worker
+  worker.init();
 };
 
-// handle request and response
-app.handleReqRes = handleReqRes;
-//  start the server
-app.createServer();
+app.init();
+
+//  exports app
+module.exports = app;
